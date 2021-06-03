8784597c84ce94b4c5e40613e68d2d830613aca3
handagyeong <80438961+handagyeong@users.noreply.github.com>
2021-06-03 17:51:49 +0900

    t
    f

---
+++ 013/js/main.js
@@ -0,0 +1,12 @@
+const $scrollToTop = document.querySelector(".scrollToTop");
+
+$scrollToTop.addEventListener("click", function () {
+  window.scroll({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
+        <img class="btn scrollToTop"
+            src="https://purepng.com/public/uploads/large/purepng.com-watermelonfruitsslicewatermelonmelonsummer-fruitrefreshing-9815251818421yuvm.png"
+            alt="수박" />
--- 롯데시네마 project/js/main.js
+++ 롯데시네마 project/js/main.js
@@ -22,3 +22,20 @@ $(window).on('scroll',function(){
     else{ 
         $('#menuline2').removeClass('active'); } });
 
+const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'vertical',
+    autoplay: true,
+    loop: true,
+    
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    
+    
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+    });
