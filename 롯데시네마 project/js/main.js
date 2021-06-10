0bcaf1d903c872963dc737db1bd7e90b280846f4
handagyeong <80438961+handagyeong@users.noreply.github.com>
2021-06-10 21:24:01 +0900

    d
    d

--- 롯데시네마 project/js/main.js
+++ 롯데시네마 project/js/main.js
@@ -1,3 +1,4 @@
+// 인기영화 슬라이드 우측 상단 실시간 로드
 function showClock() {
     var currentDate = new Date();
     var divClock = document.getElementById("clock");
@@ -15,13 +16,16 @@ function showClock() {
     setTimeout(showclock,1000);
     
 }
+
+// 아래로 스크롤시 메인메뉴 색 반전으로 드러나기
 $(window).on('scroll',function(){ 
     if($(window).scrollTop()){
          $('#menuline2').addClass('active0');
          $('.main_sub').addClass('active0');
     }
     else{ 
-        $('#menuline2').removeClass('active0'); } });
+        $('#menuline2').removeClass('active0');
+        $('.main_sub').removeClass('active0'); } });
 
 // 공지 수직 슬라이드
 var swiper3 = new Swiper('.swiper3', {
@@ -58,29 +62,29 @@ const swiper1 = new Swiper(".swiper1", {
     },
     });
 
-$(".swiper-button-stop").click(function(){
+    // 메인슬라이드 정지/play 버튼
+$(document).on("click",".swiper-button-stop",function(){
     swiper1.autoplay.stop();
     $('#stopbtn').addClass('active');
     $('#startbtn').addClass('active');
     });
 
-$(".swiper-button-start").click(function(){
+$(document).on("click",".swiper-button-start",function(){
     swiper1.autoplay.play();
     $('#stopbtn').removeClass('active');
     $('#startbtn').removeClass('active');
     });
 
-
+// 인기영화슬라이드
 var swiper = new Swiper(".swiper2", {
     slidesPerView: 5,
-    // spaceBetween: 10,
     navigation: {
         nextEl: ".nextbtn2",
         prevEl: ".prevbtn2"
     }
     });
 
-
+// 호버->메뉴 드러남
 $(function(){
     $('.main_title1').mouseenter(function(){
       $('.main_sub1').css('visibility','visible');
@@ -124,4 +128,10 @@ $(function(){
     $('.main_title5').mouseleave(function(){
       $('.main_sub5').css('visibility','hidden');
     });
+});
+
+
+// x 버튼 클릭시 상단 광고 사라짐 -jquery
+$(document).on("click","#close",function(){
+  $('.ad').hide();
 });
